f787d48aa502a7cf4065d58c10bc3d3ac6d8dda9
R. 'Patches' S <patches@nullcorp.org>
2017-12-03 21:18:57 -0500

    Missed a 'secrets' portion.

--- agents/github.py
+++ agents/github.py
@@ -1,6 +1,5 @@
 import aiohttp
-from os.path import expanduser
-from commands import readFile
+import config
 
 
 async def getCommits(user, repo):
@@ -8,15 +7,13 @@ async def getCommits(user, repo):
     Simple method to retrieve the commits for a users repository on github.
     '''
     async with aiohttp.ClientSession() as session:
-        secrets = await readFile(expanduser('~/projects/JARVIS/secrets'))
-
         # Hard code the formatting.
         url = f'https://api.github.com/repos/{user}/{repo}/commits'
 
         # The rate limiting is much better for authenticated users.
         auth = {
             'User-Agent': 'JARVIS/v2 (https://github.com/PatchesPrime/JARVIS)',
-            'Authorization': 'token {}'.format(secrets['github'])
+            'Authorization': 'token {}'.format(config.github)
         }
 
         async with session.get(url, headers=auth) as response:
