58dcf1c31d1219c7bc346a6184c32f3c700d1e92
R. "Patches" S <patches@nullcorp.org>
2018-05-29 09:33:46 -0400

    Added check so we always have a full subscriber.

--- commands.py
+++ commands.py
@@ -175,6 +175,10 @@ async def addWeatherSub(db, target, zipcode, *, caller=None):
     same = await getSAMECode(zipcode)  # Get the SAME.
     same = same.split()[-1]  # Chop it up.
 
+    if not await db.subscribers.find_one({'user': str(target)}):
+        logging.debug("addWeatherSub target not a subscriber, adding..")
+        await addSubscriber(db, target, caller=caller)
+
     result = await db.subscribers.update_one(
         {'user': str(target)},
         {'$push': {'same_codes': same}},
