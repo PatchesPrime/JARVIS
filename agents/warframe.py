382bb14d90a17e4e64b541e74ee989dbfec92076
R. "Patches" S <patches@nullcorp.org>
2018-08-04 13:36:15 -0400

    Basic agent, still testing.

--- agents/warframe.py
+++ agents/warframe.py
@@ -2,7 +2,7 @@ import aiohttp
 import json
 import asyncio
 import logging
-from datetime import timedelta
+from datetime import timedelta, datetime
 from socket import create_connection
 import msgpack
 
@@ -27,13 +27,22 @@ async def get_warframe():
                 # Shorthand
                 mission_rewards = alert['MissionInfo']['missionReward']
 
+                # Split into two lines, PEP8 pls.
+                expires = int(alert['Expiry']['$date']['$numberLong']) / 1000
+                expires = datetime.fromtimestamp(expires)
+
                 # TODO: clean all this up.
                 # Is it a counted or no?
                 field = mission_rewards.get('countedItems')
                 if field:
                     for item in field:
                         if any(thing in item['ItemType'] for thing in watched):
-                            results.append(item['ItemType'])
+                            results.append(
+                                {
+                                    'item': item['ItemType'].split('/')[-1],
+                                    'expires': expires
+                                }
+                            )
                 else:
                     # We don't concern ourselves with credits-only alerts.
                     if 'items' not in mission_rewards.keys():
@@ -41,6 +50,46 @@ async def get_warframe():
 
                     for item in mission_rewards['items']:
                         if any(thing in item for thing in watched):
-                            results.append(item)
+                            results.append(
+                                {
+                                    'item': item.split('/')[-1],
+                                    'expires': expires
+                                }
+                            )
 
             return results
+
+
+async def agent(db, *, freq=timedelta(minutes=5)):
+    while True:
+        logging.debug('Checking Warframe Alerts..')
+        check = await get_warframe()
+
+        query = {'warframe': {'$exits': True}}
+        qfilter = {'user': 1, 'warframe': 1}
+        async for sub in db.subscribers.find(query, qfilter):
+            if len(check) > 0:
+                msg = (
+                    'Test Message, Please Ignore!',
+                    check
+                )
+
+                # Payload.
+                payload = {
+                    'to': sub['user'],
+                    'msg': '\n'.join(msg),
+                    'type': 'warframe',
+                }
+
+                # Pass the payload to Jarvis.
+                sock = create_connection(('192.168.1.200', 8888))
+                sock.send(msgpack.packb(payload))
+                sock.close()
+
+        # Reeeeee, 79 characters.
+        logging.debug(
+            'agents.warframe sleeping {} seconds'.format(
+                freq.total_seconds()
+            )
+        )
+        await asyncio.sleep(freq.total_seconds())
